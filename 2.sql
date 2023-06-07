3d9a7c7f8da1b83e2bfd6454e6128ff4a8582c9e
Denowsky <den.san404@gmail.com>
2023-06-07 18:49:15 +0300

    Исправления во второй задаче

--- 2.sql
+++ 2.sql
@@ -3,12 +3,12 @@ DELIMITER $$
     RETURNS TEXT(45)
     DETERMINISTIC
     BEGIN
-    DECLARE evennum INT default 0;
+    DECLARE evennum INT default 2;
     DECLARE result TEXT default "";
 
-    WHILE evennum < num DO
-        SET evennum = evennum + 2;
+    WHILE evennum BETWEEN 0 AND num DO
         SET result = CONCAT(result, evennum, ", ");
+        SET evennum = evennum + 2;
     END WHILE;
     return result;
 END $$
